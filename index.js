76fb177c252b869be322e768d1ae0a1bde5a7fe8
Karan Chaudhary <karanchaudhary009@gmail.com>
2020-03-07 10:20:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+
+const port    = 3000;
+
+const app     = express ();
+
+app.get ('/', (req, res) => res.send ('send top 10 apps'));
+
+app.listen (port, () => console.log (`App listening on port ${port}`));
